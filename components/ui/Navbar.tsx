caf84d3855be5e97a824caa63fe6c5b2f3e8f034
Alex <acuevasferras@gmail.com>
2023-03-18 19:35:45 -0400

    Allow pictures from pokemos api site

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { Spacer, Text, useTheme } from "@nextui-org/react";
+import Image from "next/image";
 
 interface NavbarProps {}
 
@@ -17,14 +18,23 @@ export const Navbar: FunctionComponent<NavbarProps> = () => {
         backgroundColor: theme?.colors.gray100.value,
       }}
     >
+      <Image
+        src={
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/132.png"
+        }
+        alt="icono de la app"
+        width={70}
+        height={70}
+      />
+      {/* "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/132.png" */}
       <Text color="white" h2>
         P
       </Text>
       <Text color="white" h3>
         okemon
       </Text>
-      <Spacer css={{flex: 1}}/>
-      <Text color="white">Favoritos</Text> 
+      <Spacer css={{ flex: 1 }} />
+      <Text color="white">Favoritos</Text>
     </div>
   );
 };
